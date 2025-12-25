37f8bc0006e841e24061aa26813197e7e62a55a5
WinDanesz <31292708+windanesz@users.noreply.github.com>
2025-12-25 22:24:38 +0100

    fix: Fixed crash on dedicated servers

--- src/main/java/com/windanesz/menhir/network/PacketOpenBirthsignSelectionGUI.java
+++ src/main/java/com/windanesz/menhir/network/PacketOpenBirthsignSelectionGUI.java
@@ -1,14 +1,11 @@
 package com.windanesz.menhir.network;
 
+
 import com.windanesz.menhir.Menhir;
 import io.netty.buffer.ByteBuf;
-import net.minecraft.client.Minecraft;
-import net.minecraft.entity.player.EntityPlayer;
 import net.minecraftforge.fml.common.network.simpleimpl.IMessage;
 import net.minecraftforge.fml.common.network.simpleimpl.IMessageHandler;
 import net.minecraftforge.fml.common.network.simpleimpl.MessageContext;
-import net.minecraftforge.fml.relauncher.Side;
-import net.minecraftforge.fml.relauncher.SideOnly;
 
 /**
  * Packet sent from server to client to open the birthsign selection GUI
@@ -31,16 +28,8 @@ public class PacketOpenBirthsignSelectionGUI implements IMessage {
 	
 	public static class Handler implements IMessageHandler<PacketOpenBirthsignSelectionGUI, IMessage> {
 		@Override
-		@SideOnly(Side.CLIENT)
 		public IMessage onMessage(PacketOpenBirthsignSelectionGUI message, MessageContext ctx) {
-			// Execute on the main client thread
-			Minecraft.getMinecraft().addScheduledTask(() -> {
-				EntityPlayer player = Minecraft.getMinecraft().player;
-				if (player != null) {
-					Menhir.proxy.openBirthsignSelectionGUI(player);
-				}
-			});
-			
+			Menhir.proxy.handleOpenBirthsignSelectionGUI(message, ctx);
 			return null; // No response packet
 		}
 	}
